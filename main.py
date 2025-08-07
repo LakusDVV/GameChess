c561434998dd533c11e467b7cc5ee5c42cf4d16f
Lakus_DVV <evondvlad@gmail.com>
2025-08-07 18:53:30 +0300

    First dey chess project

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import raylibpy as rl
 
+rows = 8               # Кол-во строк
+cols = 8               # Кол-во столбцов
+tile_size = 50         # Размер квадрата (пиксели)
+width = cols * tile_size
+height = rows * tile_size
+
 # Инициализация окна
-rl.init_window(600, 600, "Chess")
+rl.init_window(width=width, height=height, title="Chess")
 rl.set_target_fps(60)
 
 while not rl.window_should_close():
     rl.begin_drawing()
+    rl.clear_background(rl.RAYWHITE)
 
-
-    rl.draw_text("Привет от raylib!", 10, 10, 20, rl.DARKGRAY)
-    rl.draw_circle(300, 300, 50, rl.RED)
+    # Рисуем доску
+    for y in range(rows):
+        for x in range(cols):
+            color = rl.BLACK if (x + y) % 2 == 0 else rl.WHITE
+            rl.draw_rectangle(x * tile_size, y * tile_size, tile_size, tile_size, color)
 
     rl.end_drawing()
 
